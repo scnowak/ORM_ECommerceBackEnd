4e9b37b5ae9acc320abbac3697257230ed2c6e69
scnowak <143449653+scnowak@users.noreply.github.com>
2023-12-09 21:45:09 -0600

    added put and delete routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -67,10 +67,45 @@ router.post("/", (req, res) => {
 
 router.put("/:id", (req, res) => {
   // update a category by its `id` value
+
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCategoryData => {
+      if (!dbCategoryData) {
+        res.status(404).json({message: "FOUND NO CATEGORIES WITH THE ID!"});
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
+
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCategoryData => {
+      if (!dbCategoryData){
+        res.status(404).json({message: "FOUND NO CATEGORIES WITH THE ID!"});
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
+
